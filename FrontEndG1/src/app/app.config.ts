c06a97c62d126c3e9aa8062a248944d4021ccf91
VladimirFabri98 <vfficial98@gmail.com>
2025-09-22 13:51:50 +0200

    Vezbe 11 - Sortiranje i Paginacija

--- FrontEndG1/src/app/app.config.ts
+++ FrontEndG1/src/app/app.config.ts
@@ -1,8 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-  ]
+    importProvidersFrom(MatDatepickerModule),
+    importProvidersFrom(MatNativeDateModule),
+  ],
 };
--- FrontEndG1/src/app/components/main/artikl-component/artikl-component.ts
+++ FrontEndG1/src/app/components/main/artikl-component/artikl-component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,45 +6,48 @@ import { Artikl } from '../../../models/artikl';
 import { ArtiklService } from '../../../services/artikl-service';
 import { MatDialog } from '@angular/material/dialog';
 import { ArtiklDialog } from '../../dialogs/artikl-dialog/artikl-dialog';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-artikl-component',
-  imports: [MatTableModule, MatIconModule, MatToolbarModule],
+  imports: [MatTableModule, MatIconModule, MatToolbarModule, MatSortModule, MatPaginatorModule],
   templateUrl: './artikl-component.html',
-  styleUrl: './artikl-component.css'
+  styleUrl: './artikl-component.css',
 })
-export class ArtiklComponent implements OnInit{
- 
-  displayedColumns = ['id','naziv','proizvodjac','actions'];
-  dataSource !: MatTableDataSource<Artikl>;
-  
-  constructor(private artiklService:ArtiklService, private dialog: MatDialog){}
+export class ArtiklComponent implements OnInit {
+  displayedColumns = ['id', 'naziv', 'proizvodjac', 'actions'];
+  dataSource!: MatTableDataSource<Artikl>;
+  constructor(private artiklService: ArtiklService, private dialog: MatDialog) {}
 
-   ngOnInit(): void {
+  @ViewChild(MatSort, { static: false }) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator!: MatPaginator;
+
+  ngOnInit(): void {
     this.loadData();
   }
 
-  public loadData():void{
+  public loadData(): void {
     this.artiklService.getAllArtikls().subscribe({
-      next: data => {
+      next: (data) => {
         console.log(data);
         this.dataSource = new MatTableDataSource<Artikl>(data);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       },
-      error: error => {
+      error: (error) => {
         console.log(error.message);
-      }
-    })
+      },
+    });
   }
 
-  public openDialog(flag:number, id?:number, naziv?:String, proizvodjac?:String):void{
-    const ref = this.dialog.open(ArtiklDialog, {data: {id,naziv,proizvodjac}});
+  public openDialog(flag: number, id?: number, naziv?: String, proizvodjac?: String): void {
+    const ref = this.dialog.open(ArtiklDialog, { data: { id, naziv, proizvodjac } });
     ref.componentInstance.flag = flag;
-    ref.afterClosed().subscribe(
-      response => {
-        if(response === 1){
-          this.ngOnInit();
-        }
+    ref.afterClosed().subscribe((response) => {
+      if (response === 1) {
+        this.ngOnInit();
       }
-    )
+    });
   }
 }
